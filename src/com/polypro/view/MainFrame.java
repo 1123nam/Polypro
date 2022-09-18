91e4403af811b17a82ecea2f6372ee5955070614
LAPTOP LENOVO <LAPTOP LENOVO@HOAINAM-03>
2022-09-18 18:37:21 +0700

    fix loi icon lan 2

--- src/com/polypro/view/MainFrame.java
+++ src/com/polypro/view/MainFrame.java
@@ -695,7 +695,7 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        
+        //thoat chuong trinh
         exitProgram();
         
     }//GEN-LAST:event_jMenuItem4ActionPerformed
